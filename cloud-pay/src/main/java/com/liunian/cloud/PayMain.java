6cc3cc46d9ea2499d0b4e0f8ac699bb9a58cf3bf
liulianmt <yangkjava@163.com>
2020-03-14 22:38:44 +0800

    第一个子模块创建

---
+++ cloud-pay/src/main/java/com/liunian/cloud/PayMain.java
@@ -0,0 +1,21 @@
+package com.liunian.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ProjectName: cloud2020
+ * @PackageName: com.liunian.cloud
+ * @ClassName: PayMain
+ * @Description: pay模块 启动类
+ * @author: liunian
+ * @date: 2020/3/14 22:34
+ * @Copyright: GitHub Copyright (c) 2020
+ */
+@SpringBootApplication
+public class PayMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayMain.class, args);
+    }
+}
